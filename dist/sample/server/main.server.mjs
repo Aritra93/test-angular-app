dcf61f606d8cab149806f4474ae4a10445c7060c
Aritra Dey <aritradey1993@gmail.com>
2024-02-28 01:59:44 -0500

    design commit

--- dist/sample/server/main.server.mjs
+++ dist/sample/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-HYTEB3WU.mjs";import"./chunk-5ONB7SQF.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-OLJV4KIO.mjs";import"./chunk-5ONB7SQF.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
